8b7790669354a5ab076a126810d03bdcb0e5ce94
Raimundo Flores de nexoemprendia <creacion.activa@gmail.com>
2026-02-03 19:41:47 -0300

    Sitio Seguro v1

---
+++ importar_logo.bat
@@ -0,0 +1,2 @@
+@echo off
+copy "C:\Users\raimu\.gemini\antigravity\brain\d9bc76d0-8a63-4eea-90bc-7800c5b66d4c\uploaded_media_1770158424681.jpg" "C:\Users\raimu\OneDrive\Escritorio\Cuatropuntas-Secure\public\logo_cuatropuntas.jpg"
