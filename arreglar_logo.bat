e4f0bbda968fd7cb0201eacccc21ea0b7b00906a
Raimundo Flores de nexoemprendia <creacion.activa@gmail.com>
2026-02-03 20:07:23 -0300

    Sitio Seguro v1

--- arreglar_logo.bat
+++ arreglar_logo.bat
@@ -1,6 +1,19 @@
 @echo off
 cd public
-if exist "Logo.jpg.png" ren "Logo.jpg.png" "logo.png"
-if exist "logo.jpg" ren "logo.jpg" "logo.png"
-echo Nombre arreglado!
+echo Buscando archivo de logo...
+if exist "Logo.jpg.png" (
+    ren "Logo.jpg.png" "logo.png"
+    echo [EXITO] Renombrado Logo.jpg.png a logo.png
+) else (
+    echo [INFO] No encontre Logo.jpg.png, quizas ya se llama logo.png?
+)
+
+if exist "logo.jpg" (
+    ren "logo.jpg" "logo.png"
+    echo [EXITO] Renombrado logo.jpg a logo.png
+)
+
+echo.
+echo LISTO! Ahora ejecuta "subir_a_github.bat"
+time /t
 pause
